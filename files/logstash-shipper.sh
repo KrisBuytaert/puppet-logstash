124518e63fe987e5bab9a14a33ba3ce322a857ed
Kris Buytaert <github@krisbuytaert.be>
2011-12-29 15:50:05 +0100

    Elastic search with matching version requirement Bunch of ugly scripts to launch everything

---
+++ files/logstash-shipper.sh
@@ -0,0 +1,2 @@
+java -jar logstash-1.0.17-monolithic.jar agent -f /etc/logstash/shipper.conf  
+
---
+++ files/logstash-web.sh
@@ -0,0 +1,2 @@
+cd /usr/local/logstash/
+java -jar logstash-1.0.17-monolithic.jar web --backend elasticsearch://127.0.0.1:9300/
