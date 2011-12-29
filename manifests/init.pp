d1b0880860215c522103d7e3a8696589fbe3478b
Kris Buytaert <github@krisbuytaert.be>
2011-12-29 15:50:05 +0100

    Elastic search with matching version requirement Bunch of ugly scripts to launch everything

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,48 @@
+
+class logstash::common {
+
+  file {
+    "/usr/local/logstash/":
+      ensure   => "directory";
+    "/etc/logstash/":
+      ensure   => "directory";
+    "/var/log/logstash/":
+      ensure   => "directory",
+      recurse  => true;
+  }
+
+
+  # Obviously I abused fpm to create a logstash package and put it on my repository 
+  package {
+    "logstash":
+      ensure => "latest";
+  }
+}
+
+class logstash {
+  include logstash::common
+}
+
+
+
 class logstash::simple { 
-		
-	package { 'java-1.6.0-openjdk':
-					ensure => 'installed';
-		}
-	file {
-		"/usr/local/logstash/":
-			ensure => "directory",
-			recurse => true;
-		"/usr/local/logstash/logstash.jar":
-			ensure => "file",
-			source => "puppet:///modules/logstash/logstash-1.0.14-monolithic.jar",
-			require => File['/usr/local/logstash'];
-		"/etc/logstash/":
-			ensure => "directory";
-		"/var/log/logstash/": 
-			ensure => "directory";
-	}
-
-	file { 
-		"/etc/logstash/simple.conf":
-			ensure => "file",
-			source => "puppet:///modules/logstash/simple.conf",
-			require => File['/etc/logstash'];
-		"/etc/init.d/logstash-simple":
-			ensure => "file", 
-			mode => 0744,
-			source => "puppet:///modules/logstash/logstash-simple",
-			require => [ File['/usr/local/logstash/logstash.jar'], File['/var/log/logstash/'] ],
-		}
 
 
+  file {
+    "/usr/local/logstash/logstash.jar":
+      ensure  => "file",
+      source  => "puppet:///modules/logstash/logstash-1.0.14-monolithic.jar",
+      require => File['/usr/local/logstash'];
+    "/etc/logstash/simple.conf":
+      ensure  => "file",
+      source  => "puppet:///modules/logstash/simple.conf",
+      require => File['/etc/logstash'];
+    "/etc/init.d/logstash-simple":
+      ensure  => "file", 
+      mode    => 0744,
+      source  => "puppet:///modules/logstash/logstash-simple",
+      require => [ File['/usr/local/logstash/logstash.jar'], File['/var/log/logstash/'] ],
+  }
 
 #	service { 'logstash-simple':
 #			ensure => 'running',
@@ -43,7 +56,41 @@ class logstash::simple {
 
 
 
-class logstash::server {
+class logstash::shipper {
+
+  file { 
+    '/etc/logstash/shipper.conf':
+      ensure  => 'file',
+      group   => '0',
+      mode    => '644',
+      owner   => '0',
+      source  => "puppet:///modules/logstash/shipper.conf";
+  }
+
 
 
 }
+
+
+class logstash::server {
+
+
+  file { 
+    '/etc/logstash/indexer.conf':
+      ensure  => 'file',
+      group   => '0',
+      mode    => '644',
+      owner   => '0',
+      source  => "puppet:///modules/logstash/indexer.conf";
+  }
+}
+
+class logstash::web {
+
+  # Most probably only the rc script goes inhere .. 
+
+
+
+}
+
+
