95df12a94c35bafa0d8d347d8e3fc8cc0b46f34f
kbuytaert <kbuytaert@981703c8-b323-4728-b969-ff5e5cfaebfe>
2012-01-09 12:05:22 +0000

    Initial commit of module logstash
    git-svn-id: http://svn.dev.unifiedpost.com/repos/it/puppet/modules/logstash/trunk@2 981703c8-b323-4728-b969-ff5e5cfaebfe

---
+++ logstash/files/logstash-server.sh
@@ -0,0 +1,3 @@
+cd /usr/local/logstash/
+java -jar logstash-1.0.17-monolithic.jar agent -f /etc/logstash/indexer.conf 
+
---
+++ logstash/manifests/init.pp
@@ -0,0 +1,162 @@
+
+class logstash::common {
+
+  file {
+    "/usr/local/logstash/":
+      ensure   => "directory";
+    "/etc/logstash/":
+      ensure   => "directory";
+    "/usr/local/logstash/bin/":
+      ensure   => "directory";
+    "/usr/local/logstash/lib/":
+      ensure   => "directory";
+    "/usr/local/logstash/conf":
+      ensure   => "directory";
+    "/usr/local/logstash/logs":
+      ensure   => "directory";
+    "/var/log/logstash/":
+      ensure   => "directory",
+      recurse  => true;
+  }
+
+
+  # Obviously I abused fpm to create a logstash package and put it on my repository 
+  package {
+    "logstash":
+      ensure => "latest";
+  }
+}
+
+class logstash {
+  include logstash::common
+}
+
+
+
+
+
+class logstash::shipper {
+
+  file { 
+    '/etc/logstash/shipper.conf':
+      ensure  => 'file',
+      group   => '0',
+      mode    => '644',
+      owner   => '0',
+      source  => "puppet:///modules/logstash/shipper.conf";
+  }
+
+
+  
+  file {
+    '/etc/rc.d/init.d/logstash-shipper':
+      ensure => 'file',
+      group  => '0',
+      mode   => '755',
+      owner  => '0',
+      source => 'puppet:///modules/logstash/logstash-shipper' ;
+  }
+
+
+
+
+  file {
+    '/usr/local/logstash/conf/shipper-wrapper.conf':
+      ensure   => 'file',
+      group    => '0',
+      mode     => '644',
+      owner    => '0',
+      source   => 'puppet:///modules/logstash/shipper-wrapper.conf';
+    }
+
+service { 'logstash-shipper':
+  ensure    => 'running',
+  hasstatus => 'true',
+  enable    => 'true',
+}
+
+
+
+}
+
+
+class logstash::server {
+
+
+  file { 
+    '/etc/logstash/indexer.conf':
+      ensure  => 'file',
+      group   => '0',
+      mode    => '644',
+      owner   => '0',
+      source  => "puppet:///modules/logstash/indexer.conf";
+  }
+
+
+ file {
+    '/etc/rc.d/init.d/logstash-server':
+      ensure => 'file',
+      group  => '0',
+      mode   => '755',
+      owner  => '0',
+      source => 'puppet:///modules/logstash/logstash-server' ;
+  }
+
+
+
+
+  file {
+    '/usr/local/logstash/conf/server-wrapper.conf':
+      ensure   => 'file',
+      group    => '0',
+      mode     => '644',
+      owner    => '0',
+      source   => 'puppet:///modules/logstash/server-wrapper.conf';
+    }
+
+service { 'logstash-server':
+  ensure    => 'running',
+  hasstatus => 'true',
+  enable    => 'true',
+}
+
+
+
+}
+
+class logstash::web {
+
+
+ file {
+    '/etc/rc.d/init.d/logstash-web':
+      ensure => 'file',
+      group  => '0',
+      mode   => '755',
+      owner  => '0',
+      source => 'puppet:///modules/logstash/logstash-web' ;
+  }
+
+
+
+
+  file {
+    '/usr/local/logstash/conf/web-wrapper.conf':
+      ensure   => 'file',
+      group    => '0',
+      mode     => '644',
+      owner    => '0',
+      source   => 'puppet:///modules/logstash/web-wrapper.conf';
+    }
+
+
+service { 'logstash-web':
+  ensure    => 'running',
+  hasstatus => 'true',
+  enable    => 'true',
+}
+
+
+
+}
+
+
