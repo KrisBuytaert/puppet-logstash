24552960d5eadf5da5d0d01ae4d89ad4b94038b5
Trey Darley <tre.darley@unifiedpost.com>
2012-03-06 16:23:20 +0000

    merging my rsyslog changes which i naively did on the test environment back into svn
    git-svn-id: http://svn.dev.unifiedpost.com/repos/it/puppet/modules/logstash/trunk@7 981703c8-b323-4728-b969-ff5e5cfaebfe

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,181 +1,153 @@
-
 class logstash::common {
-
   file {
     '/usr/local/logstash/':
-      ensure   => 'directory';
+      ensure   => directory;
     '/etc/logstash/':
-      ensure   => 'directory';
+      ensure   => directory;
     '/usr/local/logstash/bin/':
-      ensure   => 'directory';
+      ensure   => directory;
     '/usr/local/logstash/lib/':
-      ensure   => 'directory';
+      ensure   => directory;
     '/usr/local/logstash/conf':
-      ensure   => 'directory';
+      ensure   => directory;
     '/usr/local/logstash/logs':
-      ensure   => 'directory';
+      ensure   => directory;
     '/var/log/logstash/':
-      ensure   => 'directory',
+      ensure   => directory,
       recurse  => true;
   }
-
-
   # Obviously I abused fpm to create a logstash package and put it on my
   # repository
-  package {
-    'logstash':
-      ensure => 'latest';
-  }
+  package { 'logstash': ensure => latest; }
 }
 
 class logstash {
   include logstash::common
 }
 
-
-
-
-
 class logstash::shipper (
-  $logstash_server ='localhost',
+  $logstash_server = 'localhost',
   $verbose = 'no',
-  $jarname ='logstash-1.1.0-monolithic.jar',
+  $jarname = 'logstash-1.1.0-monolithic.jar',
   # TODO This needs refactoring :)
   $logfiles = '"/var/log/messages", "/var/log/syslog", "/var/log/*.log"'
 )
 {
-
   file {
     '/etc/logstash/shipper.conf':
-      ensure  => 'file',
+      ensure  => file,
       group   => '0',
       mode    => '0644',
       owner   => '0',
       content => template('logstash/shipper.conf.erb'),
+      notify  => Service['logstash-shipper'],
   }
-
-
   file {
     '/etc/rc.d/init.d/logstash-shipper':
-      ensure => 'file',
+      ensure => file,
       group  => '0',
       mode   => '0755',
       owner  => '0',
-      source => 'puppet:///modules/logstash/logstash-shipper' ;
+      notify  => Service['logstash-shipper'],      
+      source => 'puppet:///modules/logstash/logstash-shipper';
   }
-
-
-
-
   file {
     '/usr/local/logstash/conf/shipper-wrapper.conf':
-      ensure   => 'file',
+      ensure   => file,
       group    => '0',
       mode     => '0644',
       owner    => '0',
+      notify  => Service['logstash-shipper'],
       content  => template('logstash/shipper-wrapper.conf.erb');
   }
-
   service { 'logstash-shipper':
-    ensure    => 'running',
-    hasstatus => 'true',
-    enable    => 'true',
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
   }
-
-
-
   file { '/etc/logrotate.d/syslog':
-    ensure   => 'file',
+    ensure   => file,
     group    => '0',
     mode     => '0644',
     owner    => '0',
     source   => 'puppet:///modules/logstash/syslog.logrotate';
   }
-
-
-
 }
-
 
 class logstash::server(
   $verbose = 'no',
-  $jarname ='logstash-1.1.0-monolithic.jar'
+  $jarname = 'logstash-1.1.0-monolithic.jar'
 )
 {
-
   file {
     '/etc/logstash/indexer.conf':
-      ensure  => 'file',
+      ensure  => file,
       group   => '0',
       mode    => '0644',
       owner   => '0',
+      notify  => Service['logstash-server'],
       source  => 'puppet:///modules/logstash/indexer.conf';
   }
-
   file {
     '/etc/rc.d/init.d/logstash-server':
-      ensure => 'file',
+      ensure => file,
       group  => '0',
       mode   => '0755',
       owner  => '0',
+      notify  => Service['logstash-server'],
       source => 'puppet:///modules/logstash/logstash-server' ;
   }
-
-
-
-
   file {
     '/usr/local/logstash/conf/server-wrapper.conf':
-      ensure   => 'file',
+      ensure   => file,
       group    => '0',
       mode     => '0644',
       owner    => '0',
+      notify  => Service['logstash-server'],
       content  => template('logstash/server-wrapper.conf.erb');
   }
-
-  service { 'logstash-server':
-    ensure    => 'running',
-    hasstatus => 'true',
-    enable    => 'true',
+  file {
+    '/etc/logstash/grok_patterns':
+      source  => 'puppet:///modules/logstash/grok_patterns',
+      group   => '0',
+      mode    => '0644',
+      owner   => '0',
+      notify  => Service['logstash-server'],
+      ensure  => directory,
+      replace => true,
+      purge   => true,
+      recurse => true;
   }
-
-
-
+  service { 'logstash-server':
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
+  }
 }
 
-class logstash::web ($jarname ='logstash-1.1.0-monolithic.jar') {
-
-
+class logstash::web ($jarname ='logstash-1.1.0-monolithic.jar')
+{
   file {
     '/etc/rc.d/init.d/logstash-web':
-      ensure => 'file',
+      ensure => file,
       group  => '0',
       mode   => '0755',
       owner  => '0',
+      notify  => Service['logstash-web'],
       source => 'puppet:///modules/logstash/logstash-web' ;
   }
-
-
-
-
   file {
     '/usr/local/logstash/conf/web-wrapper.conf':
-      ensure   => 'file',
+      ensure   => file,
       group    => '0',
       mode     => '0644',
       owner    => '0',
+      notify  => Service['logstash-web'],
       content  => template('logstash/web-wrapper.conf.erb');
   }
-
-
   service { 'logstash-web':
-    ensure    => 'running',
-    hasstatus => 'true',
-    enable    => 'true',
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
   }
-
-
-
 }
-
-
