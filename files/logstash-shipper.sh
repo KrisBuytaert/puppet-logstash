54281f60fa7e9e37cb515a5be3ee614c28e73c25
Kris Buytaert <sdog@inuits.be>
2012-11-05 15:37:50 +0100

    Cleaninup the obsolete files

--- files/logstash-shipper.sh
+++
@@ -1,2 +0,0 @@
-java -jar logstash-1.0.17-monolithic.jar agent -f /etc/logstash/shipper.conf  
-
--- files/logstash-web.sh
+++
@@ -1,2 +0,0 @@
-cd /usr/local/logstash/
-java -jar logstash-1.0.17-monolithic.jar web --backend elasticsearch://127.0.0.1:9300/
